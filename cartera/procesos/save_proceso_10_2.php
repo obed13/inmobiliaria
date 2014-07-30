3cc900cf6c98104352263a0cd61e86427a14f82d
Obed <obekcdr@hotmail.com>
2014-07-29 19:28:06 -0700

    re-contrato
    es el ciclo para el re-contrato

--- cartera/procesos/save_proceso_10_2.php
+++ cartera/procesos/save_proceso_10_2.php
@@ -20,15 +20,15 @@
     $contrato = $inst->fetch_array();
 
     if ($contrato['nuevo_contrato'] == 'si') {
-        $query='SELECT ifnull(max(consec),0)+1 FROM campana WHERE id_cartera='.$id_cartera;
+        $query='SELECT ifnull(max(consec),0)+1 FROM proceso_cartera WHERE id_cartera='.$id_cartera;
         $cons = $conexion->query($query);
         $consec = $cons->fetch_array();
         $sqlnuevo = "INSERT INTO proceso_cartera(id_cartera,consec,nom_cartera,fecha,id_proceso,fecha_inicio,fecha_entrega,comment_preliminar,cita_propiedad,comment_seguimiento,acuerdo_previo
-            ,acuerdo_comment,precio_dueno,nuevo_precio,precio_sugerido,archivo,revision_cond_preliminar,foto_preliminar,elab_contrato
-            ,recabar_firmas,firma_aviso_privacidad,contrato_inicio,contrato_fin,nueva_fecha,recabar_doc_mls,poner_lona,crm) 
+            ,acuerdo_comment) 
                     VALUES ('".$contrato['id_cartera']."','".$consec['consec']."','".$contrato['nom_cartera']."','".$contrato['fecha']."',8,'".$contrato['fecha_inicio']."','".$contrato['fecha_entrega']."'
-                        ,'".$contrato['comment_preliminar']."','".$contrato['cita_propiedad']."','".$contrato['comment_seguimiento']."','".$contrato['acuerdo_previo']."','".$contrato['acuerdo_comment']."'
-                        ,'".$contrato['precio_dueno']."','no','".$contrato['precio_sugerido']."','".$contrato['archivo']."','".$contrato['revision_cond_preliminar']."','".$contrato['foto_preliminar']."'
+                        ,'".$contrato['comment_preliminar']."','".$contrato['cita_propiedad']."','".$contrato['comment_seguimiento']."','".$contrato['acuerdo_previo']."','".$contrato['acuerdo_comment']."')";
+        $sqlnuevo2 = "INSERT INTO proceso_cartera (precio_dueno,nuevo_precio,precio_sugerido,archivo,revision_cond_preliminar,foto_preliminar,elab_contrato
+            ,recabar_firmas,firma_aviso_privacidad,contrato_inicio,contrato_fin,nuevo_contrato,nueva_fecha,recabar_doc_mls,poner_lona,crm) VALUES ('".$contrato['precio_dueno']."','no','".$contrato['precio_sugerido']."','".$contrato['archivo']."','".$contrato['revision_cond_preliminar']."','".$contrato['foto_preliminar']."'
                         ,'".$contrato['elab_contrato']."','".$contrato['recabar_firmas']."','".$contrato['firma_aviso_privacidad']."','".$contrato['contrato_inicio']."','".$contrato['contrato_fin']."'
                         ,'no','".$contrato['nueva_fecha']."','".$contrato['recabar_doc_mls']."','".$contrato['poner_lona']."','".$contrato['crm']."')";
 
